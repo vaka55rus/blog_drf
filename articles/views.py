8501b4be65bba4fa0183ca828de84e1bb59ebcd9
Apansenko Roman <romis_97@mail.ru>
2021-01-20 18:23:51 +0600

    + filter by author

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.db.models import Q
 
 from .models import Article
 from .serializers import ArticleListSerializers, ArticleDetailSerializers
@@ -6,7 +7,10 @@ from .serializers import ArticleListSerializers, ArticleDetailSerializers
 
 class ArticleListView(generics.ListAPIView):
     serializer_class = ArticleListSerializers
-    queryset = Article.objects.filter(published=True)
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(Q(published=True) | Q(author=self.request.user))
+        return queryset
 
 
 class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
